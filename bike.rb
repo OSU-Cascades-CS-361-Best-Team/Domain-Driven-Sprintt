504e6816c935fdd5089f9cd1d20effbce58bda72
Adrianna Guevarra <guevarra@oregonstate.edu>
2020-02-19 10:39:03 -0800

    add commas to attr_accessor and add availability and inventory attributes Refers #1

--- bike.rb
+++ bike.rb
@@ -1,12 +1,28 @@
 class Bike
 
   #declared getter and setters 
-  attr_accessor :weight :color :size 
+  attr_accessor :weight, :color, :size, :availability
   
   def initialize
     @weight = weight
     @color = color
     @size = size
+    @availability = availability
+    @inventory = inventory
   end
+
+  def repair
+  end
+
+  def move
+  end
+
+  def check_availability
+  end
+
+  def update_inventory
+  end
+
+
 
 end
