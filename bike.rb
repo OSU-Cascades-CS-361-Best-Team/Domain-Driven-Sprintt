335d1728d395d1203a20d095eab595aab6e2c7a2
Adrianna Guevarra <guevarra@oregonstate.edu>
2020-02-19 10:48:26 -0800

    Refactor Bike Class Refers #1

--- bike.rb
+++ bike.rb
@@ -9,21 +9,9 @@ class Bike
     @color = color
     @size = size
     @availability = availability
-    @inventory = inventory
   end
 
   def repair
   end
-
-  def move
-  end
-
-  def check_availability
-  end
-
-  def update_inventory
-  end
-
-
 
 end
