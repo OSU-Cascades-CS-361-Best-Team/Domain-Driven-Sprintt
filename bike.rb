059a545870f4568cbf01251c5de39eb2d0240fdb
Adrianna Guevarra <guevarra@oregonstate.edu>
2020-02-19 10:53:37 -0800

    update bike class to have description attribute Refers #1

--- bike.rb
+++ bike.rb
@@ -2,13 +2,14 @@ class Bike
 
 
   #declared getter and setters 
-  attr_accessor :weight, :color, :size, :availability
+  attr_accessor :weight, :color, :size, :availability, :label, :description
   
-  def initialize
+  def initialize weight, color, size, availability, description
     @weight = weight
     @color = color
     @size = size
     @availability = availability
+    @description = description
   end
 
   def repair
