502e3a58bfa1efa1d6bc92cc3dcb41764241d6f7
Adrianna Guevarra <guevarra@oregonstate.edu>
2020-02-19 11:10:02 -0800

    create 5 bike objects Refers #1

--- main.rb
+++ main.rb
@@ -1,3 +1,10 @@
 require_relative 'bike.rb'
 require_relative 'inventory.rb'
 require_relative 'check_inventory.rb'
+
+
+bike1 = Bike.new(10, "pink", 10, True, "Pink Bike")
+bike2 = Bike.new(10, "pink", 10, False, "Pink Bike")
+bike3 = Bike.new(10, "pink", 10, True, "Pink Bike")
+bike4 = Bike.new(10, "pink", 10, False, "Pink Bike")
+bike5 = Bike.new(10, "pink", 10, True, "Pink Bike")
