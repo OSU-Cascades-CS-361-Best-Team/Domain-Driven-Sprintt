fc959e744546eb844a718b15b31721c04ad4eabf
Shayla Lane <lanesha@oregonstate.edu>
2020-02-22 10:54:50 -0800

    Customer: Add class variables.     References #2.

--- customer.rb
+++ customer.rb
@@ -1,8 +1,8 @@
 class Customer
 
-	attr_accessor :cust_id
-
-	def initialize(cust_id)
+	attr_accessor :cust_id, f_name, l_name
+	 
+	def initialize(cust_id, f_name, l_name)
 		@cust_id = cust_id
 	end
 
