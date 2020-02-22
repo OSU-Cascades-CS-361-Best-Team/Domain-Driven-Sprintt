fa3b27398037bce87a3975c616cd8e5c08dad487
Jackson <jackson.cutler@gmail.com>
2020-02-22 11:54:29 -0800

    Main: Flush out main References #2

--- main.rb
+++ main.rb
@@ -5,7 +5,11 @@ require_relative 'customer'
 require_relative 'bicycle_rental_workflow'
 
 bike = Bike.new(1, 100, true, 100)
+nilBike = NilBike.new()
 customer = Customer.new(1, 'John', 'Doe')
-rental = Bicycle_Rental.new(customer, bike)
+rental = Bicycle_Rental.new(customer, nilBike)
+inventory = Inventory.new()
+inventory.add(bike)
 
-workflow = Bicycle_Rental_Workflow.new(rental)
+workflow = Bicycle_Rental_Workflow.new(inventory)
+workflow.run()
