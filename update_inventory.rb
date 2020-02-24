5b56f58ce12246ea11688f12e55a5dffa31e57ef
Jackson <jackson.cutler@gmail.com>
2020-02-24 11:52:20 -0800

    Brainstorm class functionality Refers #5

--- update_inventory.rb
+++ update_inventory.rb
@@ -1,6 +1,13 @@
 
 class update_inventory
-    def initialize
+    def initialize(bikes)
+    	# bikes is a list (or just a single bike)
     end
+
+
+    # update inventory function that takes bikes
+    # for each bike, update inventory ?
+
+
 end
 
