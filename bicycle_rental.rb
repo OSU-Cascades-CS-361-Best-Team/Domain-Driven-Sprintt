7a06a9cc339068d135ad55647325caf9e3a9c45e
Shayla Lane <lanesha@oregonstate.edu>
2020-02-26 10:48:00 -0800

    Extend member variables References #6:

--- bicycle_rental.rb
+++ bicycle_rental.rb
@@ -1,15 +1,17 @@
 class BicycleRental
 
-    attr_accessor :rental_id, :bike, :customer
+    attr_accessor :rental_id, :bike, :customer, :price
 
     @@current_rental_id = 0
 
-    def initialize (customer, bike, cost)
+    def initialize (customer, bike, price, pick_up, drop_off)
         @customer = customer
         @bike = bike
         @rental_id = @@current_rental_id
         @@current_rental_id += 1
-        @cost = cost
+        @price = price
+        @pick_up = pick_up
+        @drop_off = drop_off
     end
 
 end
