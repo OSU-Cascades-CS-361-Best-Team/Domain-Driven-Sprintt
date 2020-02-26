83d0a0d2e6bb6b0b2d1c24a3b5aa179b6f6d9925
Adrianna Guevarra <guevarra@oregonstate.edu>
2020-02-26 10:42:13 -0800

    initialize update_inventory attributes Refers #5

--- update_inventory.rb
+++ update_inventory.rb
@@ -1,12 +1,16 @@
 
 class update_inventory
-    def initialize(bikes)
-    	# bikes is a list (or just a single bike)
+    def initialize(inventory, bike)
+        # bikes is a list (or just a single bike)
+        @inventory = inventory 
+        @bike = bike
     end
 
 
     # update inventory function that takes bikes
     # for each bike, update inventory ?
+    def update(inventory, bike)
+        
 
 
 end
