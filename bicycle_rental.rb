f28a6540dfe8c1e0a1d2f8addb64116c5534d0a7
Shayla Lane <lanesha@oregonstate.edu>
2020-02-26 10:39:25 -0800

    Implement incrementing id References #6

--- bicycle_rental.rb
+++ bicycle_rental.rb
@@ -1,11 +1,15 @@
 class BicycleRental
 
-    attr_accessor :rental_id, :bike
+    attr_accessor :rental_id, :bike, :customer
 
-    def initialize (customer, bike)
+    @@current_rental_id = 0
+
+    def initialize (customer, bike, cost)
         @customer = customer
         @bike = bike
-        @rental_id = 0
+        @rental_id = @@current_rental_id
+        @@current_rental_id += 1
+        @cost = cost
     end
 
 end
