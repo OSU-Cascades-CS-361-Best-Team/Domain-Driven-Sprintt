e9e016dd939fd400dbfc258832a502fdf187ada0
Carson-Key <keyc@oregonstate.edu>
2020-02-29 12:41:54 -0800

    Add runable rental report Refers #6

--- main.rb
+++ main.rb
@@ -4,6 +4,8 @@ require_relative 'check_inventory'
 require_relative 'bicycle_rental'
 require_relative 'customer'
 require_relative 'bicycle_rental_workflow'
+require_relative 'generate_report'
+require_relative 'rental_report'
 
 customer1 = Customer.new(1, 'John', 'Doe')
 customer2 = Customer.new(1, 'Jane', 'Doe')
@@ -27,8 +29,8 @@ inventory.add(bike5)
 
 checkInventory.pretty_print(inventory.bikes)
 
-rental1 = BicycleRental.new(customer1, nilBike)
-rental2 = BicycleRental.new(customer2, nilBike)
+rental1 = BicycleRental.new(customer1, nilBike, "$1,000", "02/29/2001", "Never")
+rental2 = BicycleRental.new(customer2, nilBike, "$100,000,000", "02/28/2020", "02/29/2020")
 
 workflow = BicycleRentalWorkflow.new()
 
@@ -38,3 +40,11 @@ workflow.run(inventory, rental2)
 puts "\n"
 puts rental1.bike.print_attributes
 puts rental2.bike.print_attributes
+
+rental_report1 = RentalReport.new(rental1)
+rental_report2 = RentalReport.new(rental2)
+
+generate_report = GenerateReport.new
+
+generate_report.pretty_print(rental_report1)
+generate_report.pretty_print(rental_report2)
