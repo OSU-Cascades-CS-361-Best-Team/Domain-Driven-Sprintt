6c2b57b6f8e09f8ebe7dd71f8f1fe6fb55f5f7c4
Carson-Key <keyc@oregonstate.edu>
2020-02-29 11:30:06 -0800

    Create initializer Refers #6

--- rental_report.rb
+++ rental_report.rb
@@ -1,5 +1,12 @@
 class RentalReport
-    def initialize(bicycle_rental)
-        
+    def initialize(rental)
+        @customer_id = rental.get_customer_id
+        @customer_name = rental.get_customer_name
+        @drop_off_date = rental.drop_off
+        @pick_up_date = rental.pick_up
+        @price = rental.price
+        @rental_id = rental.id
+        @bike_id = rental.get_bike_id
+        @bike_description = rental.get_bike_description
     end
 end
