f4cdf836f1f9e935c80de9f6ca22938e0602e991
Carson-Key <keyc@oregonstate.edu>
2020-02-29 11:31:05 -0800

    Add bike/customer getters Refers #6

--- bicycle_rental.rb
+++ bicycle_rental.rb
@@ -1,6 +1,6 @@
 class BicycleRental
 
-    attr_accessor :rental_id, :bike, :customer, :price
+    attr_accessor :rental_id, :bike, :customer, :price, :pick_up, :drop_off
 
     @@current_rental_id = 0
 
@@ -14,7 +14,19 @@ class BicycleRental
         @drop_off = drop_off
     end
 
+    def get_customer_id
+        customer.cust_id
+    end
+
+    def get_customer_name
+        customer.f_name + " " + customer.l_name
+    end
+
+    def get_bike_id
+        bike.id
+    end
+
+    def get_bike_description
+        bike.description
+    end
 end
-
-
-
