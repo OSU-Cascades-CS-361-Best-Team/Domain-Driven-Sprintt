c7b0b4d06a56d8a5f746800a4d89980eda014683
Shayla Lane <lanesha@oregonstate.edu>
2020-03-04 11:20:15 -0800

    Add print attributes method References #9

--- kayak.rb
+++ kayak.rb
@@ -1,8 +1,12 @@
 class Kayak
-    def initialize(id, description, price, weight)
+    def initialize(id, description, price, weight, availability)
         @id = id
         @description = description
         @price = price
         @weight = weight
+    end
+
+    def print_attributes
+        print @id, " ", @description , " " , @price , " " , @weight , " " ,@availability , "\n"
     end
 end
