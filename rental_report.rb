84c9272d73a829e6e1d5e34c47f2a3f686eb0614
Jackson <jackson.cutler@gmail.com>
2020-03-04 11:51:46 -0800

    Fix whitespace Refers #10

--- rental_report.rb
+++ rental_report.rb
@@ -1,21 +1,23 @@
 class RentalReport
-    def initialize(rental)
-        @customer_id = rental.get_customer_id
-        @customer_name = rental.get_customer_name
-        @drop_off_date = rental.drop_off
-        @pick_up_date = rental.pick_up
-        @price = rental.price
-        @rental_id = rental.id
-        @item_id = rental.get_item_id
-        @item_description = rental.get_item_description
-    end
+    
+  def initialize(rental)
+    @customer_id = rental.get_customer_id
+    @customer_name = rental.get_customer_name
+    @drop_off_date = rental.drop_off
+    @pick_up_date = rental.pick_up
+    @price = rental.price
+    @rental_id = rental.id
+    @item_id = rental.get_item_id
+    @item_description = rental.get_item_description
+  end
 
-    def generate
-        @rental_id + "|" + @customer_id + "|" + @customer_name + "|" + @item_id + "|" + @item_description + "|" + @pick_up_date + "|" + @drop_off_date + "|" + @price
-    end
+  def generate
+    @rental_id + "|" + @customer_id + "|" + @customer_name + "|" + @item_id + "|" + @item_description + "|" + @pick_up_date + "|" + @drop_off_date + "|" + @price
+  end
 
-    def pretty_print
-        puts "rental_id | customer_id | customer_name | bike_id | bike_description | pick_up_date | drop_off_date | price"
-        puts @rental_id.to_s + " | " + @customer_id.to_s + " | " + @customer_name + " | " + @item_id.to_s + " | " + @item_description + " | " + @pick_up_date + " | " + @drop_off_date + " | " + @price.to_s
-    end
+  def pretty_print
+    puts "rental_id | customer_id | customer_name | bike_id | bike_description | pick_up_date | drop_off_date | price"
+    puts @rental_id.to_s + " | " + @customer_id.to_s + " | " + @customer_name + " | " + @item_id.to_s + " | " + @item_description + " | " + @pick_up_date + " | " + @drop_off_date + " | " + @price.to_s
+  end
+
 end
