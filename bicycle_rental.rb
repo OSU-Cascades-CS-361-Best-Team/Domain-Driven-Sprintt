74affa74d6d9015b1115dcae58c6571d5ee71edb
Jackson <jackson.cutler@gmail.com>
2020-03-04 11:32:13 -0800

    Tweak rental id assignment Refers #10

--- bicycle_rental.rb
+++ bicycle_rental.rb
@@ -2,16 +2,16 @@ class BicycleRental
 
     attr_accessor :id, :bike, :customer, :price, :pick_up, :drop_off
 
-    @@current_id = 0
+    @@current_id = 1
 
     def initialize (customer, bike, price, pick_up, drop_off)
         @customer = customer
         @bike = bike
         @id = @@current_id
-        @@current_id += 1
         @price = price
         @pick_up = pick_up
         @drop_off = drop_off
+        @@current_id += 1
     end
 
     def get_customer_id
