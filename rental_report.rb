c320ac9122293fc2f53866e7a899bea44c07b158
Jackson <jackson.cutler@gmail.com>
2020-03-04 11:43:14 -0800

    Rename bike to item in rental report Refers #10

--- rental_report.rb
+++ rental_report.rb
@@ -6,16 +6,16 @@ class RentalReport
         @pick_up_date = rental.pick_up
         @price = rental.price
         @rental_id = rental.id
-        @bike_id = rental.get_bike_id
-        @bike_description = rental.get_bike_description
+        @item_id = rental.get_item_id
+        @item_description = rental.get_item_description
     end
 
     def generate
-        @rental_id + "|" + @customer_id + "|" + @customer_name + "|" + @bike_id + "|" + @bike_description + "|" + @pick_up_date + "|" + @drop_off_date + "|" + @price
+        @rental_id + "|" + @customer_id + "|" + @customer_name + "|" + @item_id + "|" + @item_description + "|" + @pick_up_date + "|" + @drop_off_date + "|" + @price
     end
 
     def pretty_print
         puts "rental_id | customer_id | customer_name | bike_id | bike_description | pick_up_date | drop_off_date | price"
-        puts @rental_id.to_s + " | " + @customer_id.to_s + " | " + @customer_name + " | " + @bike_id.to_s + " | " + @bike_description + " | " + @pick_up_date + " | " + @drop_off_date + " | " + @price.to_s
+        puts @rental_id.to_s + " | " + @customer_id.to_s + " | " + @customer_name + " | " + @item_id.to_s + " | " + @item_description + " | " + @pick_up_date + " | " + @drop_off_date + " | " + @price.to_s
     end
 end
