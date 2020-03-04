c9d39f24665ea2a320702840a195daa90e66ccf0
Jackson <jackson.cutler@gmail.com>
2020-03-04 11:47:29 -0800

    Fix whitespace Refers #10

--- bicycle_rental.rb
+++ bicycle_rental.rb
@@ -1,32 +1,33 @@
 class BicycleRental
 
-    attr_accessor :id, :item, :customer, :price, :pick_up, :drop_off
+  attr_accessor :id, :item, :customer, :price, :pick_up, :drop_off
 
-    @@current_id = 1
+  @@current_id = 1
 
-    def initialize (customer, item, price, pick_up, drop_off)
-        @customer = customer
-        @item = item
-        @id = @@current_id
-        @price = price
-        @pick_up = pick_up
-        @drop_off = drop_off
-        @@current_id += 1
-    end
+  def initialize (customer, item, price, pick_up, drop_off)
+    @customer = customer
+    @item = item
+    @id = @@current_id
+    @price = price
+    @pick_up = pick_up
+    @drop_off = drop_off
+    @@current_id += 1
+  end
 
-    def get_customer_id
-        customer.cust_id
-    end
+  def get_customer_id
+      customer.cust_id
+  end
 
-    def get_customer_name
-        customer.f_name + " " + customer.l_name
-    end
+  def get_customer_name
+      customer.f_name + " " + customer.l_name
+  end
 
-    def get_item_id
-        item.id
-    end
+  def get_item_id
+      item.id
+  end
 
-    def get_item_description
-        item.description
-    end
+  def get_item_description
+      item.description
+  end
+  
 end
