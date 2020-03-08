d1aeb84272e5d48f03a0a71ff0a9955292a22fb4
Shayla Lane <lanesha@oregonstate.edu>
2020-03-08 15:14:34 -0700

    Refactor initializer to not require rental object References #10

--- rental_report.rb
+++ rental_report.rb
@@ -1,18 +1,18 @@
 class RentalReport
     
-  def initialize(rental)
-    @customer_id = rental.get_customer_id
-    @customer_name = rental.get_customer_name
-    @drop_off_date = rental.drop_off
-    @pick_up_date = rental.pick_up
-    @price = rental.price
-    @rental_id = rental.id
-    @item_id = rental.get_item_id
-    @item_description = rental.get_item_description
+  def initialize(customer, rental_item, rental_price, rental_id)
+    @customer_id = customer.get_customer_id
+    @customer_name = customer.get_customer_name
+    @drop_off_date = customer.drop_off
+    @pick_up_date = customer.pick_up
+    @item_id = rental_item.get_item_id
+    @item_description = rental_item.get_item_description
+    @rental_id = rental_id
+    @rental_price = rental_price
   end
 
   def generate
-    @rental_id.to_s + " | " + @customer_id.to_s + " | " + @customer_name + " | " + @item_id.to_s + " | " + @item_description + " | " + @pick_up_date + " | " + @drop_off_date + " | " + @price.to_s
+    @rental_id.to_s + " | " + @customer_id.to_s + " | " + @customer_name + " | " + @item_id.to_s + " | " + @item_description + " | " + @pick_up_date + " | " + @drop_off_date + " | " + @rental_price.to_s
   end
 
   def pretty_print
