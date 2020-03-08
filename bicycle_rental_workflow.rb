c6c66fa39636240266ff6f9d4eaa7ea71c4f0582
Jackson <jackson.cutler@gmail.com>
2020-03-08 13:59:57 -0700

    Fix whitespace Refers #10

--- bicycle_rental_workflow.rb
+++ bicycle_rental_workflow.rb
@@ -3,8 +3,8 @@ class BicycleRentalWorkflow
   attr_accessor :item, :rental
 
   def initialize(item, rental)
-  	@item = item
-  	@rental = rental
+    @item = item
+    @rental = rental
   end
 
   def run()
